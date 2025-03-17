ccee55f055307158cbd37fc7527ba2990521baf8
cokirmg <100722843+cokirmg@users.noreply.github.com>
2025-03-17 19:01:45 +0100

    First Push

---
+++ Source/ChasingTwilight/ChasingTwilight.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ChasingTwilight.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ChasingTwilight, "ChasingTwilight" );
+ 
---
+++ Source/ChasingTwilight/ChasingTwilightGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ChasingTwilightGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AChasingTwilightGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AChasingTwilightGameMode();
+};
+
+
+
